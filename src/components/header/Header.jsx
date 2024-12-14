0bcf9952ee4c033c429846edeb50800a28d804e6
Fifcham <body.bertrand@gmail.com>
2024-12-14 18:17:37 +0100

    feat: Add header with language selection and login
    Add a header component including language selection
    dropdown and login button for improved user experience.
    Improve header styling and responsiveness.
    Include Font Awesome icons for enhanced visual appeal.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,25 @@
-import './header.scss'
-import neflixlogo from '../../assets/netflixheader.png'
+import "./header.scss";
+import neflixlogo from "../../assets/netflixheader.png";
+import languageicone from "../../assets/languageicone.png";
 function Header() {
   return (
-    <div className='header'>
-
-    <div className='header__container'>
-
+    <div className="header">
+      <div className="header__container">
         <img src={neflixlogo} alt="neflixlogo" />
-        <ul>
-            <li>langages</li>
-            <li>login</li>
-        </ul>
-    </div>
+        <div className="language-login-container">
+          <div className="language-container">
+            <img src={languageicone} alt="languageicone" />
 
+            <select name="language" id="language">
+              <option value="fr">Français</option>
+              <option value="en">English</option>
+            </select>
+          </div>
+          <button className="login-button">S&apos;identifier</button>
+        </div>
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default Header
+export default Header;
