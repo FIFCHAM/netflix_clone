d81611e10ba308441364fbebae0b71308bc26e1f
Fifcham <body.bertrand@gmail.com>
2024-12-24 17:30:39 +0100

    feat: Enhance Netflix clone UI
    Implement a new first section with improved styling
    Add a subscription form and descriptive content
    Improve overall visual appeal and user experience

--- src/components/first-section/Firstsection.jsx
+++ src/components/first-section/Firstsection.jsx
@@ -1,7 +1,35 @@
 import "./firstsection.scss";
+import chevronright from "../../assets/chevron-right-solid.svg";
 function FirstSection() {
-    return (
-        <section className="first-section"></section>
-    )
+  return (
+    <>
+      <section className="first-section" />
+      <div className="first-section-content">
+        <h1 className="first-section-title">
+          Films et séries en illimité, et bien plus
+        </h1>
+        <h2 className="first-section-description">
+          À partir de 5,99 €. Annulable à tout moment.
+        </h2>
+        <form action="" className="first-section-form">
+          <h3>
+            Prêt à regarder Netflix ? Saisissez votre adresse e-mail pour vous
+            abonner ou réactiver votre abonnement.
+          </h3>
+          <input
+            type="email"
+            placeholder="Email"
+            className="first-section-form-input"
+          />
+          <div className="form-submit">
+            <button type="submit">
+              Commencer
+              <img src={chevronright} alt="chevron-right" />
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
-    export default FirstSection;
+export default FirstSection;
