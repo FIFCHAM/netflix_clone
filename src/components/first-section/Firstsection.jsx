747bd0bfe244ec6eaf29f1de7caa955ffa74307d
Fifcham <body.bertrand@gmail.com>
2024-12-25 14:51:59 +0100

    feat: Improve Netflix clone UI
    Enhance the visual appeal and user experience of the
    Netflix clone's home page.  Improve the layout and
    styling of the header and first section components.
    Add a new stylesheet for improved organization.

--- src/components/first-section/Firstsection.jsx
+++ src/components/first-section/Firstsection.jsx
@@ -2,20 +2,19 @@ import "./firstsection.scss";
 import chevronright from "../../assets/chevron-right-solid.svg";
 function FirstSection() {
   return (
-    <>
-      <section className="first-section" />
-      <div className="first-section-content">
-        <h1 className="first-section-title">
-          Films et séries en illimité, et bien plus
-        </h1>
-        <h2 className="first-section-description">
-          À partir de 5,99 €. Annulable à tout moment.
-        </h2>
-        <form action="" className="first-section-form">
-          <h3>
-            Prêt à regarder Netflix ? Saisissez votre adresse e-mail pour vous
-            abonner ou réactiver votre abonnement.
-          </h3>
+    <div className="first-section-content">
+      <h1 className="first-section-title">
+        Films et séries en illimité, et bien plus
+      </h1>
+      <h2 className="first-section-description">
+        À partir de 5,99 €. Annulable à tout moment.
+      </h2>
+      <form action="" className="first-section-form">
+        <h3>
+          Prêt à regarder Netflix ? Saisissez votre adresse e-mail pour vous
+          abonner ou réactiver votre abonnement.
+        </h3>
+        <div className="form-input-container">
           <input
             type="email"
             placeholder="Email"
@@ -27,9 +26,9 @@ function FirstSection() {
               <img src={chevronright} alt="chevron-right" />
             </button>
           </div>
-        </form>
-      </div>
-    </>
+        </div>
+      </form>
+    </div>
   );
 }
 export default FirstSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import "./home.scss";
 import Header from "../components/header/Header";
 import FirstSection from "../components/first-section/Firstsection";
 function Home() {
   return (
-    <>
+    
+    <section className="first-section" >
       <Header />
       <FirstSection />
-    </>
+
+    </section>
+  
   );
 }
 
